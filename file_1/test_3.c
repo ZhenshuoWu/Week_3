d86719a87702fce03220fe8447cbc59d36874355
ZhenshuoWu <146674125+ZhenshuoWu@users.noreply.github.com>
2023-10-13 10:58:30 +0000

    friday

---
+++ file_1/test_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+
+    for (a = -4; a < 4; a++)
+    {
+        printf ("a is equal to %d\n", a);
+    }
+    printf ("finished the loop and a is equal to %d\n", a);
+
+    return (0);
+}
